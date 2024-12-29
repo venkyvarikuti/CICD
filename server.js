e7e710ca02ad2431085887450640c6f0390f5905
venkyvarikuti <varikutivenkatesh2024cse@gmail.com>
2024-12-29 12:18:12 +0530

    commiting the mongoose changes

--- server.js
+++ server.js
@@ -7,11 +7,6 @@ const app = express();
 // Middleware
 app.use(express.json());
 
-// MongoDB Connection
-// mongoose
-//   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => console.log('Connected to MongoDB'))
-//   .catch((err) => console.error('Error connecting to MongoDB:', err));
 
 
 // Start the Server
